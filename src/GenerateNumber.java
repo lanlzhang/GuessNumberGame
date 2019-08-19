2f55b0aa79f312e055e6f2cde7ccc4f39c247a52
Lanlan Zhang and Ioana Barcan <lanzhang+ioana.barcan@thoughtworks.com>
2019-08-19 14:49:09 +0800

    [lanlan] - commit the new changes

--- src/GenerateNumber.java
+++ src/GenerateNumber.java
@@ -3,11 +3,11 @@ import java.util.Random;
 public class GenerateNumber {
     public int[] generateNumberBySystem() {
         Random rand = new Random();
-        int[] randArray = {10,10,10,10};
+        int[] randArray = {10, 10, 10, 10};
 
-        for (int i = 0; i < randArray.length ; i++) {
+        for (int i = 0; i < randArray.length; i++) {
             int temp = rand.nextInt(9);
-            while (temp == randArray[0] || temp == randArray[1] || temp == randArray[2] || temp == randArray[3]){
+            while (temp == randArray[0] || temp == randArray[1] || temp == randArray[2] || temp == randArray[3]) {
                 temp = rand.nextInt(9);
             }
             randArray[i] = temp;
